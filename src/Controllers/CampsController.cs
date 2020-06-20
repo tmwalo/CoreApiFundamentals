36fa237e2037349efef90538096d92915cdcabbd
tmwalo <vuyotm@gmail.com>
2020-06-20 14:51:48 +0200

    change: Get action in Camps controller returns IActionResult

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -9,9 +9,10 @@ namespace CoreCodeCamp.Controllers
     [Route("api/[controller]")]
     public class CampsController : ControllerBase
     {
-        public object Get()
+        [HttpGet]
+        public IActionResult Get()
         {
-            return new { Moniker = "ATL2018", Name = "Atlanta Code Camp" };
+            return Ok(new { Moniker = "ATL2018", Name = "Atlanta Code Camp" });
         }
     }
 }
