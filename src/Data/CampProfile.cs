e50725098f38f5450772988c1bab5b111e5bb60b
tmwalo <vuyotm@gmail.com>
2020-06-21 12:00:10 +0200

    change: allow mapping from CampModel to Camp

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -12,7 +12,8 @@ namespace CoreCodeCamp.Data
         public CampProfile()
         {
             this.CreateMap<Camp, CampModel>()
-                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+                .ReverseMap();
         }
     }
 }
