c1856f924d48c0750f90250249376cca7c38d3bf
tmwalo <vuyotm@gmail.com>
2020-06-24 00:06:53 +0200

    add: multiple versioning methods

--- src/Startup.cs
+++ src/Startup.cs
@@ -30,7 +30,9 @@ namespace CoreCodeCamp
           opt.AssumeDefaultVersionWhenUnspecified = true;
           opt.DefaultApiVersion = new ApiVersion(1, 1);
           opt.ReportApiVersions = true;
-          opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
+          opt.ApiVersionReader = ApiVersionReader.Combine(
+              new HeaderApiVersionReader("X-Version"),
+              new QueryStringApiVersionReader("ver", "version"));
       });
 
       services.AddMvc(opt => opt.EnableEndpointRouting = false)
