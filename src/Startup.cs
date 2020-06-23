13c81826e3f58e69ad13474c3a8832952826b76a
tmwalo <vuyotm@gmail.com>
2020-06-24 00:30:55 +0200

    add: Talks controller versioning conventions

--- src/Startup.cs
+++ src/Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -34,6 +36,12 @@ namespace CoreCodeCamp
           //opt.ApiVersionReader = ApiVersionReader.Combine(
           //    new HeaderApiVersionReader("X-Version"),
           //    new QueryStringApiVersionReader("ver", "version"));
+
+          opt.Conventions.Controller<TalksController>()
+            .HasApiVersion(new ApiVersion(1, 0))
+            .HasApiVersion(new ApiVersion(1, 1))
+            .Action(c => c.Delete(default(string), default (int)))
+            .MapToApiVersion(1, 1);
       });
 
       services.AddMvc(opt => opt.EnableEndpointRouting = false)
